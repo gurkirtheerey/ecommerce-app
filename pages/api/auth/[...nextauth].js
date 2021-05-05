4a53cd2b751adeff0569f06a374977dc4ba77474
gurkirtheerey <gheerey12@gmail.com>
2021-05-04 19:00:44 -0700

    remove client keys

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,9 +4,8 @@ import Providers from "next-auth/providers";
 export default NextAuth({
   providers: [
     Providers.Google({
-      clientId:
-        "295726433247-r6nval576epuudm3c2g03900i0qlgl02.apps.googleusercontent.com",
-      clientSecret: "Jam1d-m11JCdl_MvsoB5mrIA",
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 });
